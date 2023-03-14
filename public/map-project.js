5388627080827a002bf5be3b689abf39e60469b8
Farrux Mengboyev <mengboyevfarrux@gmail.com>
2023-03-14 14:52:05 +0500

    The development of the eco-city site has begun

---
+++ public/map-project.js
@@ -0,0 +1,7 @@
+//   Sign in => autarisation // number || google account, name, surname, 
+//   Profile => meni arizalarim
+//   Infografika=> statistika.
+//   added condition => description, location, img, select(obodanashtirish)
+//   notifications, 
+//   page => sign In, home, Profile, Infografika
+//   Til
---
+++ src/root/index.js
@@ -0,0 +1,11 @@
+
+
+function Root() {
+    return (
+      <>
+        <h1 className="center font-family " >This is "Eco city" site </h1>
+      </>
+      );
+}
+
+export default Root;
